b3ccd728867a427404991be8c8bba6c3cfb42e34
gabrielbarretoufc <gabrielbarto@alu.ufc.br>
2022-05-05 11:15:46 -0300

    banco poupança

--- src/conta/criar_poupanca.py
+++ src/conta/criar_poupanca.py
@@ -1,14 +1,15 @@
 # criar poupanca
 
 from src.conta.conta_poupanca import ContaPoupanca
+from src.conta.conta import Conta
 
 class CriarPoupanca:
     if __name__ == '__main__':
-        poupanca = ContaPoupanca("21.342-7")
-        poupanca.creditar(500.87)
-        poupanca.debitar(45.00)
+        conta = Conta("21.342-7")
+        conta = ContaPoupanca(conta)
+        conta.creditar(500.87)
+        conta.debitar(45.00)
+        print(conta.get_saldo())
 
-        print(poupanca.get_saldo())
-
-        poupanca.render_juros(0.01)
-        print(poupanca.get_saldo())
+        conta.render_juros(0.01)
+        print(conta.get_saldo())
